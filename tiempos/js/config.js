2d6cd28fb5e38631d907733d6e32719e049be9da
SECeD <114856253+seced2022@users.noreply.github.com>
2026-08-03 23:30:33 +0200

    Update config.js

--- tiempos/js/config.js
+++ tiempos/js/config.js
@@ -1,1 +1,35 @@
 
+const APP_CONFIG = {
+
+    // Plataforma
+    appName: "Heidegger",
+    developer: "G6K® Innovación Electrónica",
+
+    // Colores
+    theme: {
+        primary: "#00ff66",
+        background: "#0b0b0b",
+        card: "#181818",
+        text: "#ffffff",
+        warning: "#ffb400",
+        danger: "#ff3b30"
+    },
+
+    // Logos
+    logos: {
+        heidegger: "assets/logo-heidegger.png",
+        g6k: "assets/logo-g6k.png"
+    },
+
+    // Firebase público
+    firebase: {
+        apiKey: "AIzaSyAapHF_VxFyA0DkLd4ylt_M6RiR2s6mspI",
+        authDomain: "heidegger-tiempos.firebaseapp.com",
+        databaseURL: "https://heidegger-tiempos-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "heidegger-tiempos",
+        storageBucket: "heidegger-tiempos.firebasestorage.app",
+        messagingSenderId: "358686549642",
+        appId: "1:358686549642:web:d927c0696b60f24f802f73"
+    }
+
+};
